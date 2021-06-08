5a5fb70d59939ac3cef83fbbde54d51135dc069c
MistaRae <itsamistarae@gmail.com>
2021-06-08 18:03:41 +0100

    EXTENSIONS - added borrower test to library test

---
+++ src/test/java/BorrowerTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class BorrowerTest {
+
+    private Borrower mark;
+
+    @Before
+    public void setUp(){
+        mark = new Borrower();
+    }
+
+    @Test
+    public void borrowerCanBorrow(){
+
+    }
+}
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -11,11 +11,13 @@ public class LibraryTest {
     private Book dune;
     private Book theHobbit;
     private Book learnToProgram;
+    private Borrower mark;
 
 
     @Before
     public void setUp(){
         library = new Library();
+        mark = new Borrower();
         code = new Book("Code", "Charles Petzold", "General Computing");
         dune = new Book("Dune", "Frank Herbert", "Science-Fiction");
         theHobbit = new Book("The Hobbit", "JRR Tolkien", "Fantasy");
@@ -49,6 +51,14 @@ public class LibraryTest {
         library.addBookToStock(dune);
         library.addBookToStock(theHobbit);
         assertEquals("insufficient space",library.addBookToStock(learnToProgram));
+    }
+
+    @Test
+    public void borrowerCanBorrow(){
+        library.addBookToStock(theHobbit);
+        mark.borrowBook();
+        assertEquals(1, mark.getnumOfBorrowedBooks());
+
     }
 
 
